af0ab38be3a2cb6ed18ddc3bfe8db39b0e572dbf
M4cs <maxlikescs@gmail.com>
2019-01-18 03:39:32 -0500

    Commit Files | Need Listing, Better UI, Positive Redux Results

--- catulator-app-redux/src/actions/actionType.js
+++
@@ -1,3 +0,0 @@
-export const GET_ALL_CATS = 'GET_ALL_CATS';
-export const CREATE_NEW_CAT = 'CREATE_NEW_CATS';
-export const REMOVE_CAT = 'REMOVE_CAT';
--- catulator-app-redux/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import cats from './catReducer';
-
-export default combineReducers({
-    cats: cats
-});
